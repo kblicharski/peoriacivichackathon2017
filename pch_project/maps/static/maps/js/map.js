5eb2ea55845d9b6d610fa00a0bf89baa9b9872ce
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-08-12 15:15:45 -0500

    Moves JS into its own file and cleans things up

---
+++ pch_project/maps/static/maps/js/map.js
@@ -0,0 +1,50 @@
+let map;
+
+function initMap(mapObject) {
+  map = new google.maps.Map(mapObject.mapDiv, {
+    center: {
+      lat: 40.6936,
+      lng: -89.5890
+    },
+    zoom: 13
+  });
+
+  loadData();
+}
+
+function loadData() {
+  let data = [{
+      'url': 'http://127.0.0.1:8000/static/maps/ArterialRecon.geojson',
+      'color': 'blue'
+    },
+    {
+      'url': 'http://127.0.0.1:8000/static/maps/Pavement2017.geojson',
+      'color': 'red'
+    },
+    /* {
+      'url': 'http://127.0.0.1:8000/static/maps/RampConstruction.geojson',
+      'color': 'green'
+    }, */
+    {
+      'url': 'http://127.0.0.1:8000/static/maps/ResidentialRecon.geojson',
+      'color': 'purple'
+    },
+    {
+      'url': 'http://127.0.0.1:8000/static/maps/SidewalkConstruction.geojson',
+      'color': 'orange'
+    }
+  ];
+
+  for (elem of data) {
+    map.data.loadGeoJson(elem['url']);
+    let color = 'blue';
+    map.data.setStyle(
+      {
+        fillColor: color,
+        strokeColor: color,
+        strokeWeight: 2
+      }
+    );
+  }
+}
+

54425fd79a82658766113b835172604ed14384df
Madeline Silva <mad.silva.ic@gmail.com>
2017-08-12 15:13:16 -0500

    adds uploaded kml file path to context

--- pch_project/maps/views.py
+++ pch_project/maps/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .forms import RouteForm
 from .models import Route
 
 def index(request):
+    context = {}
     if request.method == 'POST':
         form = RouteForm(request.POST, request.FILES)
         if form.is_valid():
             instance = Route(name=request.POST['name'], kml=request.FILES['kml'])
             instance.save()
+            messages.add_message(request, messages.SUCCESS, 'Successful upload!')
+            context['kml_url'] = instance.kml
     else:
         form = RouteForm()
-    return render(request, 'maps/index.html', {'form': form})
+    context['form'] = form
+    return render(request, 'maps/index.html', context)

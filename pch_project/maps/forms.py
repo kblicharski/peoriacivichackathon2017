1cfbcf21e14fc4e349c8313bba5a11827b49fa30
Madeline Silva <mad.silva.ic@gmail.com>
2017-08-12 13:47:50 -0500

    changes form a tiny bit

--- pch_project/maps/forms.py
+++ pch_project/maps/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class RouteForm(forms.Form):
-    name = forms.CharField(max_length=30, help_text='Enter a name for your route')
+    name = forms.CharField(max_length=30)
     kml = forms.FileField(help_text='Upload a .kml file')
 
